03be75fb7b90e54c71a34f60cbfbf2053f209663
nadezhda-linnik <nadezhda.linnik@ewave.com>
2023-10-17 14:45:48 +0300

    Elizabeth 1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/customer/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
